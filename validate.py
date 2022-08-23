4b6f944acf60b8ba098eafecf8c27aeadfc1fd66
Mircea Dan <mirceadan@gmail.com>
2022-08-23 08:45:28 -0700

    add some json form and yaml

---
+++ validate.py
@@ -0,0 +1,17 @@
+import os
+import json
+import yaml
+from yangson import DataModel
+os.chdir(r"/home/mircea/cgyang/models")
+
+
+dm = DataModel()
+
+print(dm.ascii_tree(), end='')
+
+
+with open('/home/mircea/cgyang/test_data.json') as infile:
+    ri = json.load(infile)
+inst = dm.from_raw(ri)
+
+print(inst.validate())
