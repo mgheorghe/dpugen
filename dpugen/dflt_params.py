5031a54c1fe3095689367502b8d295d160e1d76f
Mircea Dan <mircea-dan.gheorghe@keysight.com>
2023-03-23 10:59:07 -0700

    moved params in generic location since all code is same

--- dpugen/dflt_params.py
+++ dpugen/dflt_params.py
@@ -9,7 +9,7 @@ dflt_params = {                        # CONFIG VALUE             # DEFAULT VALU
     'PAR':                             '221.2.0.1',                # '221.2.0.1'
 
     'ENI_START':                        1,                         # 1
-    'ENI_COUNT':                        16,                        # 64
+    'ENI_COUNT':                        64,                        # 64
     'ENI_MAC_STEP':                     '00:00:00:18:00:00',       # '00:00:00:18:00:00'
     'ENI_STEP':                         1,                         # 1
     'ENI_L2R_STEP':                     1000,                      # 1000
@@ -25,7 +25,7 @@ dflt_params = {                        # CONFIG VALUE             # DEFAULT VALU
     'ACL_NSG_COUNT':                    5,                         # 5 (per direction per ENI)
     'ACL_RULES_NSG':                    1000,                      # 1000
     'IP_PER_ACL_RULE':                  128,                       # 128
-    'IP_MAPPED_PER_ACL_RULE':           128,                       # 128 (must be equal with IP_PER_ACL_RULE)
+    'IP_MAPPED_PER_ACL_RULE':           128,                       # 128 (must be equal with IP_PER_ACL_RULE) TODO: not implemented
     'IP_ROUTE_DIVIDER_PER_ACL_RULE':    16,                        # 16 (must be 2^N)
 
     'ACL_NSG_MAC_STEP':                 '00:00:00:02:00:00',
