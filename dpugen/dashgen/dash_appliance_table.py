678ed98c19b57fa10929b1d9de1d5d7da88512a2
MirceaDan <mircea-dan.gheorghe@keysight.com>
2023-06-17 00:26:54 -0700

    pointless-string-statement
    dpugen/dashgen/dash_appliance_table.py:19:8: W0105: String statement has no effect (pointless-string-statement)

--- dpugen/dashgen/dash_appliance_table.py
+++ dpugen/dashgen/dash_appliance_table.py
@@ -16,17 +16,15 @@ class Appliance(ConfBase):
     def items(self):
         print('  Generating %s ...' % os.path.basename(__file__), file=sys.stderr)
         p = self.params
-        '''
-        for eni_index, eni in enumerate(range(p.ENI_START, p.ENI_START + p.ENI_COUNT)):
-            self.num_yields += 1
-            yield {
-                'DASH_APPLIANCE_TABLE:appliance-%d' % eni: {
-                    "sip": "221.0.0.1",
-                    "vm_vni": f'{eni}'
-                },
-                'OP': 'SET'
-            }
-        '''
+        # for eni_index, eni in enumerate(range(p.ENI_START, p.ENI_START + p.ENI_COUNT)):
+        #     self.num_yields += 1
+        #     yield {
+        #         'DASH_APPLIANCE_TABLE:appliance-%d' % eni: {
+        #             "sip": "221.0.0.1",
+        #             "vm_vni": f'{eni}'
+        #         },
+        #         'OP': 'SET'
+        #     }
         self.num_yields += 1
         yield {
             'DASH_APPLIANCE_TABLE:appliance-%d' % p.ENI_START: {
