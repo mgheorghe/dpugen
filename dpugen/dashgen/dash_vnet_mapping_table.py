65d0b0b5273a6f1562acecd9f2e77c1019325aae
Mircea Dan <mircea-dan.gheorghe@keysight.com>
2024-04-23 13:20:06 -0700

    cleanup
    moves the eni vars next to eni code

--- dpugen/dashgen/dash_vnet_mapping_table.py
+++ dpugen/dashgen/dash_vnet_mapping_table.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""DASH generator for Outbound CA PA mapping table"""
 
 import os
 import sys
@@ -26,15 +27,15 @@ class Mappings(ConfBase):
         for eni_index, eni in enumerate(range(p.ENI_START, p.ENI_START + p.ENI_COUNT * p.ENI_STEP, p.ENI_STEP)):  # Per ENI
             vtep_remote = socket_inet_ntoa(struct_pack('>L', ip_int.PAR + ip_int.IP_STEP1 * eni_index))
             gateway_ip =  socket_inet_ntoa(struct_pack('>L', ip_int.GATEWAY + ip_int.IP_STEP1 * eni_index))
-            
-            eni_ip = socket_inet_ntoa(struct_pack('>L', ip_int.IP_L_START + eni_index * ip_int.IP_STEP_ENI))
-            eni_mac = str(maca(ip_int.MAC_L_START + eni_index * ip_int.MAC_STEP_ENI))
-
-            r_vni_id = eni + p.ENI_L2R_STEP
             remote_ip_a_eni = ip_int.IP_R_START + eni_index * ip_int.IP_STEP_ENI
             remote_mac_a_eni = ip_int.MAC_R_START + eni_index * ip_int.MAC_STEP_ENI
             gateway_mac = str(maca(remote_mac_a_eni))
 
+            # add mapping for ENI itself SAI_OBJECT_TYPE_ENI_ETHER_ADDRESS_MAP_ENTRY
+            eni_ip = socket_inet_ntoa(struct_pack('>L', ip_int.IP_L_START + eni_index * ip_int.IP_STEP_ENI))
+            eni_mac = str(maca(ip_int.MAC_L_START + eni_index * ip_int.MAC_STEP_ENI))
+            r_vni_id = eni + p.ENI_L2R_STEP
+            
             self.num_yields += 1
             yield {
                 'DASH_VNET_MAPPING_TABLE:vnet-%d:%s' % (r_vni_id, eni_ip): {
@@ -46,26 +47,25 @@ class Mappings(ConfBase):
                 'OP': 'SET'
             }
 
-            # 1 in 4 enis will have all its ips mapped
+            # just some prefixes may have a mapping entry
             if p.ACL_MAPPED_PER_NSG >= 1:
-                # mapped IPs
                 print(f'    mapped:eni:{eni}', file=sys.stderr)
                 for nsg_index in range(p.ACL_NSG_COUNT * 2):  # Per outbound stage
                     remote_ip_a_nsg = remote_ip_a_eni + nsg_index * ip_int.IP_STEP_NSG
                     remote_mac_a_nsg = remote_mac_a_eni + nsg_index * ip_int.MAC_STEP_NSG
+                    
                     # Per half of the rules
                     for acl_index in range(p.ACL_RULES_NSG):
                         remote_ip_a = remote_ip_a_nsg + acl_index * p.IP_PER_ACL_RULE
                         remote_mac_a = remote_mac_a_nsg + acl_index * p.IP_PER_ACL_RULE
-                        # print(acl_index)
-                        # import time
-                        # time.sleep(10)
+
                         if (acl_index % 2) == 0:
                             # Allow
                             if acl_index <= p.ACL_MAPPED_PER_NSG:
                                 for i in range(p.IP_PER_ACL_RULE):  # Per rule prefix
                                     remote_expanded_ip = socket_inet_ntoa(struct_pack('>L', remote_ip_a + i * 2))
                                     remote_expanded_mac = str(maca(remote_mac_a + i * 2))
+
                                     self.num_yields += 1
                                     yield {
                                         'DASH_VNET_MAPPING_TABLE:vnet-%d:%s' % (r_vni_id, remote_expanded_ip): {
@@ -83,10 +83,10 @@ class Mappings(ConfBase):
                             # Deny
                             pass
 
-            # 3 in 4 enis will have just mapping for gateway ip, for ip that are only routed and not mapped
+            # some prefixes will be routed through a gateway, need mapping for gateway
             elif p.ACL_MAPPED_PER_NSG == 0:
-                # routed IPs
                 print(f'    routed:eni:{eni}', file=sys.stderr)
+
                 self.num_yields += 1
                 yield {
                     'DASH_VNET_MAPPING_TABLE:vnet-%d:%s' % (r_vni_id, gateway_ip): {
